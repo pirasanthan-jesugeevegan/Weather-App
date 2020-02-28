9618d87c58867ab89fe6135ca892bbfadb15a55e
Pirasanthan Jesugeevegan <pjes0001@Synchronoss.com>
2020-02-28 22:30:03 +0000

    OpenWeather API: get request, display data.

--- js/app.js
+++ js/app.js
@@ -1,9 +1,31 @@
+var apiKey = '3aab69399bf03eca438758bf6e33d18e'
+
 //get location
 navigator.geolocation.getCurrentPosition(weather);
 
+
 function weather(position) {
-    var lat = position.coords.latitude
-    var long = position.coords.longitude
-    console.log(lat);
-    console.log(long)
+    lat = position.coords.latitude
+    long = position.coords.longitude
+    const url = 'http://api.openweathermap.org/data/2.5/weather?lat=' + lat + '&lon=' + long + '&appid=' + apiKey + '&units=metric'
+
+    fetch(url)
+        .then((resp) => resp.json())
+        .then(function (data) {
+            console.log(data)
+            let weather = data.weather[0];
+            let temp = Math.round(data.main.temp);
+            console.log(weather)
+
+            document.getElementById("location").innerHTML = data.name;
+            document.getElementById("temp").innerHTML = temp;
+        })
+        .catch(function (error) {
+            console.log(JSON.stringify(error));
+        });
+
 }
+
+
+
+
